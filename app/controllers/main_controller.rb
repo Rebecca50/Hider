4c3dd0c1d535d11cd2b6c16b88e91f004bac6e47
atrapp <andrea.trapp@gmx.net>
2014-09-17 23:15:18 +0200

    added dropdown menu for fiction and nonfiction

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,10 +6,19 @@ class MainController < ApplicationController
   def about
   end
 
-  def fiction
+  # def fiction
+  # end
+
+  def cronix
   end
 
-  def nonfiction
+  def the_law_of_return
+  end
+
+  # def nonfiction
+  # end
+
+  def spiders_of_allah
   end
 
   def freechapters
@@ -26,6 +35,6 @@ class MainController < ApplicationController
 
   def contact
   end
-    
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@ Rails.application.routes.draw do
   root "main#home"
 
   get '/about' => "main#about"
-  get '/fiction' => "main#fiction"
-  get '/nonfiction' => "main#nonfiction"
+
+  # get '/fiction' => "main#fiction"
+  get '/cronix' => "main#cronix"
+  get '/the_law_of_return' => "main#the_law_of_return"
+
+  # get '/nonfiction' => "main#nonfiction"
+  get '/spiders_of_allah' => "main#spiders_of_allah"
+
   get '/freechapters' => "main#freechapters"
   get '/buy' => 'main#buy'
   get '/forum' => 'main#forum'
   get '/blog' => 'main#blog'
   get '/contact' => 'main#contact'
-  
+
 end
