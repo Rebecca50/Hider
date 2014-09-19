216bb66b8d048cf5159645abe77daf986c4a81df
atrapp <andrea.trapp@gmx.net>
2014-09-19 17:49:14 +0200

    added avatar pics for about page, script.js and removed turbolinks causing javascript errors

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,17 @@
+
+$(function(){
+
+  $('#pic1').mouseover(function(){
+    $('#img_container').html("<img src=\"assets/avatar16.png\" \/>");
+  });
+  $('#pic2').mouseover(function(){
+    $('#img_container').html("<img src=\"assets/avatar18.png\" \/>");
+  });
+  $('#pic3').mouseover(function(){
+    $('#img_container').html("<img src=\"assets/avatar19.png\" \/>");
+  });
+  // $('#img_container').on('click', function(){
+    
+  // });
+
+})
