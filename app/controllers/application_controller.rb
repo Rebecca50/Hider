0b9649c02b94b6fa6478c09de04a05f76df0771a
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2014-10-23 15:25:33 -0600

    my local environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_user
-    return User.first
-  end
+  #def current_user
+  #  return User.first
+  #end
 
-  def login_required
-    return false
-  end
-
+  #def login_required
+  #  return false
+  #end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :rememberable,
   devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :validatable, :omniauthable, :confirmable, :lockable
+         :recoverable, :trackable, :omniauthable#, :validatable, :confirmable, :lockable
 
   has_many :posts
 end
