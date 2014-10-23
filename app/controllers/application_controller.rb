b76f0f5ad1122de69c2a7d6c1a51ef668106d896
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2014-10-23 15:28:16 -0600

    solved devise issue... was some code on the Application controller that was conflicting with current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_user
-    return User.first
-  end
+  #--- FOUND THE BUG: this was interfering with Devise current_user method...
+  #def current_user
+  #  return User.first
+  #end
 
   def login_required
     return false
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable, :rememberable,
-  devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :validatable, :omniauthable, :confirmable, :lockable
+  # :confirmable, :lockable, :timeoutable
+  devise :database_authenticatable, :registerable, :rememberable, :recoverable, :trackable, :validatable, :omniauthable, :confirmable, :lockable
 
   has_many :posts
 end
